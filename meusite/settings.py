1b1b7be8431fcc1601ad07cbea80632a3c27acad
Naptoss <tomgabinio@gmail.com>
2022-05-23 21:28:56 -0300

    FOI

--- meusite/settings.py
+++ meusite/settings.py
@@ -58,7 +58,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/TEMPLATE')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,os.path.join(BASE_DIR,'paginas')],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
